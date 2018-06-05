5af6f2c26362a6d28e7452342627394e4ee23a22
timihras <timi.hras@gmail.com>
2018-06-05 20:21:35 +0200

    Toggle Sidebar

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,24 +3,43 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Aside from '../components/Aside';
 
-export const PrivateRoute = ({
-  isAuthenticated,
-  component: Component,
-  ...rest
-}) => (
-    <Route {...rest} component={(props) => (
-      isAuthenticated ? (
-        <div>
-          <Aside />
-          <main>
-            <Component {...props} />
-          </main>
-        </div>
-      ) : (
-          <Redirect to="/" />
-        )
-    )} />
-  );
+class PrivateRoute extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidenav: false
+    }
+  }
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+      this.setState({
+        sidenav: window.innerWidth > 1200
+      });
+    }, false);
+  }
+  toggleSidenav = () => {
+    this.setState((prevState) => ({
+      sidenav: !prevState.sidenav
+    }));
+  }
+  render() {
+    const { isAuthenticated, component: Component, ...rest } = this.props;
+    return (
+      <Route {...rest} component={(props) => (
+        isAuthenticated ? (
+          <div className={this.state.sidenav ? 'sidenav--open' : ''}>
+            <Aside toggleSidenav={this.toggleSidenav} />
+            <main>
+              <Component {...props} />
+            </main>
+          </div>
+        ) : (
+            <Redirect to="/" />
+          )
+      )} />
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   isAuthenticated: !!state.auth.uid
